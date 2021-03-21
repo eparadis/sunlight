ac4705004069799220132c72a54c77d4a97dd976
Ed Paradis <eparadis@users.noreply.github.com>
2021-03-21 14:01:46 -0700

    recreate a prior codepen but in react

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [x, setX] = useState(66)
+  const [y, setY] = useState(100)
   return (
     <div className="App">
-       <p>
-          <code>HELLO WORLD!</code>
-        </p>
+      <Hello />
+      <Slider update={setX} value={x} label={"X"} min={12} max={120} />
+      <Slider update={setY} value={y} label={"Y"} min={12} max={120} />
+      <Result label={"Z"} value={calcZ(x, y)} />
     </div>
   );
+}
+
+const Hello = () => {
+  return (
+    <p>
+      <code>HELLO WORLD!</code>
+    </p>
+  )
+}
+
+const Slider = (props: { min: number, max: number, label: string, update: (v: number) => void, value: number })  => {
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    props.update(Number(event.target.value))
+  }
+  return (
+    <div className="adjustable-value">
+      <p>{props.label} = <span>{props.value}</span></p>
+      <input type='range' min={props.min} max={props.max} onChange={onChange} defaultValue={props.value}  />
+    </div>
+  )
+}
+
+const Result = (props: {label: string, value: number}) => {
+  return (
+    <div>
+      <p>{props.label} = {props.value}</p>
+    </div>
+  )
+}
+
+const calcZ = (x: number,y:number) => {
+  return x+y
 }
 
 export default App;
